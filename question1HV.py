f567304f1a72edfdfeea878a83e17e48d24df16a
d.dinesh <dinu1096@gmail.com>
2022-12-18 22:50:14 +0530

    first commit

--- question1HV.py
+++ question1HV.py
@@ -9,19 +9,19 @@
 import pandas as pd
 # x=[3,4,5,6]                 # random inputs 
 # y=[7,8,9,5]                  # random inputs 
-x=list(map(int,input("Enter first list").split()))  #inputs taken by the user - firstlist 
+x=list(map(int,input("Enter first list :").split()))  #inputs taken by the user - firstlist 
 myData=pd.Series(x)                                 #using series(x)
-y=list(map(int,input("Enter second list").split()))  #inputs taken by the user - firstlist 
+y=list(map(int,input("Enter second list :").split()))  #inputs taken by the user - firstlist 
 myData1=pd.Series(y)                                 #using series(y)
 print(myData,myData1)                                #printing myData-x and myData1-y
 operator=input("Enter  the operator :")              
-if operator=="+":
+if (operator=="+"):
     print(myData+myData1)
-elif operator=="-":
+elif (operator=="-"):
     print(myData-myData1)
-elif operator=="*":
+elif (operator=="*"):
     print(myData/myData1)
-elif operator=="%":
+elif (operator=="%"):
     print(myData%myData1)
 else:
     print("please Enter a valid opertaor")
